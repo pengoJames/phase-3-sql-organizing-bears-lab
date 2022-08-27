09266d18818d30b784b209a8780a409a76cf2d30
Pe-ngo <jamespengo95@gmail.com>
2022-08-27 11:17:05 +0300

    Completed Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 34, 'M', 'White', 'Sanguine', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 26, 'M', 'Black', 'Melancholic', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 30, 'F', 'White', 'Choleric', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 37, 'M', 'White', 'Sanguine', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 28, 'F', 'Black', 'Phlegmatic', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 24, 'M', 'Black', 'Choleric', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 36, 'F', 'Black', 'Phlegmatic', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 40, 'M', 'White', 'Melancholic', 0);
